af4cec02d951c7450a6f5af16c0ce549861f9f40
Armin Halac <armin.halac+1@gmail.com>
2021-04-05 22:35:09 +0200

    no message

--- ui.py
+++ ui.py
@@ -2,45 +2,49 @@ import bpy
 import addon_utils
 
 from rigify import rig_lists
+from rigify.utils.errors import MetarigError
+from .gamerig_generate import generate_rig
 
 class GAMERIG_OT_generate(bpy.types.Operator):
     """Generates a rig from the active metarig armature"""
 
-    bl_idname = "pose.gamerig_generate"
+    bl_idname = "pose.rigify_generate"
     bl_label = "GameRig Generate Rig"
     bl_options = {'UNDO'}
     bl_description = 'Generates a rig from the active metarig armature'
 
     def execute(self, context):
-        print("test")
+        try:
+            generate_rig(context, context.object)
+        except MetarigError as rig_exception:
+            import traceback
+            traceback.print_exc()
+
+            rigify_report_exception(self, rig_exception)
+        except Exception as rig_exception:
+            import traceback
+            traceback.print_exc()
+
+            self.report({'ERROR'}, 'Generation has thrown an exception: ' + str(rig_exception))
+        finally:
+            bpy.ops.object.mode_set(mode='OBJECT')
 
         return {'FINISHED'}
 
 
 def draw_gamerig_rigify_buttons(self, context):
-    layout = self.layout
-    obj = context.object
-
-    """
-    if not is_cloud_metarig(context.object) or obj.mode=='EDIT':
-        self.draw_old(context)
-        return
-
-    if obj.mode not in {'POSE', 'OBJECT'}:
-        return
-    """
-    layout.operator("pose.gamerig_generate", text="Generate GameRig")
-
-    draw_gamerig_generate_settings(self, context)
+    self.layout.operator("pose.gamerig_generate", text="Generate GameRig", icon='POSE_HLT')
 
 
 def draw_gamerig_generate_settings(self, context):
+    #self.layout.label(text="test")
     pass
     #for adding custom UI stuff after the Generate Button
+    
 
 
 classes = [
-    GAMERIG_OT_generate,
+    GAMERIG_OT_generate
 ]
 
 def register():
@@ -50,8 +54,7 @@ def register():
     for c in classes:
         register_class(c)
 
-    bpy.types.DATA_PT_rigify_buttons.draw_old = bpy.types.DATA_PT_rigify_buttons.draw
-    #bpy.types.DATA_PT_rigify_buttons.draw = draw_gamerig_rigify_buttons
+    bpy.types.DATA_PT_rigify_buttons.append(draw_gamerig_generate_settings)
 
 
 
@@ -62,4 +65,4 @@ def unregister():
     for c in classes:
         unregister_class(c)
 
-    bpy.types.DATA_PT_rigify_buttons.draw = bpy.types.DATA_PT_rigify_buttons.draw_old
+    bpy.types.DATA_PT_rigify_buttons.remove(draw_gamerig_generate_settings)
