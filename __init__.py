786ffa62b1147c45c2e31f376cd22d9349195802
Armin Halac <armin.halac@wooga.net>
2025-11-21 11:55:32 +0100

    Version update

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from . utils import space_switch
 rigify_info = {
     'name': "GameRig",
     'author': "Armin Halac",
-    'version': (2, 0, 0),
+    'version': (2, 1, 0),
     'blender': (4, 5, 4),
     'description': "Feature set made for game rigs",
     'doc_url': "https://github.com/Arminando/GameRig#readme",
