e86864811f7ca0158ad6454c907eb097f9aa0a0a
Armin Halac <armin.halac@wooga.net>
2025-11-24 08:28:33 +0100

    Added custom behavior for raw_copy parenting

--- rigs/game/basic/raw_copy.py
+++ rigs/game/basic/raw_copy.py
@@ -8,8 +8,18 @@ class Rig(BoneUtilityMixin, raw_copy):
 
     def parent_bones(self):
         super().parent_bones()
-        if "DEF-" in self.bones.org:
-            self.clean_def_hierarchy(self.bones.org)
+        bone_name = self.bones.org
+        if "DEF-" in bone_name:
+            # self.clean_def_hierarchy(self.bones.org)
+            old_parent = self.get_bone_parent(bone_name)
+            if old_parent:
+                target = self.find_derived_def_target(old_parent)
+                if target:
+                    self.set_bone_parent(bone_name, target)
+                else:
+                    self.generator.disable_auto_parent(bone_name)
+            else:
+                self.generator.disable_auto_parent(bone_name)
 
 def create_sample(obj):
     """ Create a sample metarig for this rig type.
